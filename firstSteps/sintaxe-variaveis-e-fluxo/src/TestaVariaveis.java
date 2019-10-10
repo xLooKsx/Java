b614ff48b78839444750afad9d8af6546f146463
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2019-10-09 22:34:57 -0300

    firstSteps
    Foram comtempladas as aulas de tipos de dados, convesões e condicional
    esta em andamento

---
+++ firstSteps/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,12 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		
+		int age = 21;
+		System.out.println("Your age is "+age+", Congratulations! :D");
+	}
+
+}
